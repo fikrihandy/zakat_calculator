85b649dc3dcb3d5259a20d41dcc5ba5e9c1f81d6
Abdullah Fikri <abdullahfikrihandi@gmail.com>
2022-07-23 11:15:43 +0700

    Main App

--- bin/main.dart
+++ bin/main.dart
@@ -5,15 +5,45 @@ import 'zakat_penghasilan.dart';
 // ==> * * * Main App Start * * * <== //
 
 void main() {
-  late bool continueApp = true;
   String exitApp = '';
+  bool continueApp = true;
+
+  // menu awal
   do {
-    print('App');
-    print("Ini akan diisi oleh Main APP");
+    int input = showMenu();
+
+    // main app
+    switch (input) {
+      case 1:
+        print('menu 1');
+        break;
+      case 2:
+        print('menu 2');
+        break;
+    }
 
     // continue using or exit from Main App
     continueApp = continueOrExit(exitApp, continueApp);
   } while (continueApp);
+}
+
+int showMenu() {
+  int input;
+  do {
+    print("1. Menu 1\n2. Menu 2");
+    stdout.write("Pilih menu (1) (2) = ");
+    try {
+      input = int.parse(stdin.readLineSync()!);
+    } on Exception {
+      stdout.write("Input salah! ");
+      input = 0;
+    }
+    if (input > 2 || input < 1) {
+      print("Menu tidak tersedia... ulangi...");
+      continue;
+    }
+  } while (input > 2 || input < 1);
+  return input;
 }
 
 // ==> * * * Main App End * * * <== //
